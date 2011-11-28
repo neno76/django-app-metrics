8560a4bebad8bf0be867e391d579279245e51177
jefftriplett <jeff.triplett@gmail.com>
2011-11-28 13:27:46 -0600

    PEP8 / PyFlakes update. Fixes and get_backend error too in move_to_mixpanel.

--- app_metrics/urls.py
+++ app_metrics/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls.defaults import * 
+from django.conf.urls.defaults import patterns, url
 
-from app_metrics.views import * 
+from app_metrics.views import *
 
-urlpatterns = patterns('', 
-        url(
-            regex   = r'^reports/$',
-            view    = metric_report_view,
-            name    = 'app_metrics_reports',
-            ),
-    )
+urlpatterns = patterns('',
+    url(
+        regex=r'^reports/$',
+        view=metric_report_view,
+        name='app_metrics_reports',
+        ),
+)
